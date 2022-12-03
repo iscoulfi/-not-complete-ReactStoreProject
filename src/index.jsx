e0cefff34748039a9ca82dadcb5347e7d213ef9c
Konstantin <iscoulfi@gmail.com>
2022-12-03 17:19:47 +0500

    markup

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
+import 'macro-css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
